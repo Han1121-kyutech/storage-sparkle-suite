4237de675e13705217690e9272833f8c999c22e2
Han <chohan161121@mail.kyutech.jp>
2026-04-03 21:27:50 +0900

    ログイン機能実装

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 // src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://rafupbgkkabuckrlwchn.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZnVwYmdia2FidWNrcmx3Y2huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMjI3OTIsImV4cCI6MjA5MDY5ODc5Mn0.93YIxm6_GCn3dAu5_1mo5ZCW6CPmU1ytIgB1FtRQNxw";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
